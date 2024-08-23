5f82aa8ef02bfa87c5d4a577ff1059df052205f9
Finu Shaheem <finushaheem@gmail.com>
2024-08-23 11:58:46 +0530

    shopping cart total price

--- src/Provider/CartProvider.tsx
+++ src/Provider/CartProvider.tsx
@@ -6,12 +6,14 @@ type CartType = {
     items: CartItem[],
     addItem: (product: Product, Size : CartItem['size']) => void;
     updateQuantity: (item: string, amount: -1 | 1) => void;
-}
+    total: number;
+};
 
 const CartContext = createContext<CartType>({
     items: [],
     addItem: () => {},
     updateQuantity: () => {},
+    total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren ) => {
@@ -48,9 +50,11 @@ const CartProvider = ({ children }: PropsWithChildren ) => {
         );
     };
         //update Quantity
+     
+    const total = items.reduce((sum, item ) => (sum += item.product.price * item.quantity), 0);
 
     return (
-        <CartContext.Provider value={{items: items, addItem, updateQuantity }}>
+        <CartContext.Provider value={{items: items, addItem, updateQuantity, total }}>
             {children}
         </CartContext.Provider>
     );
